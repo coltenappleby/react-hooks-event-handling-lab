c603aa059fdd52017bf157fb258a7d65c94b741e
Colten Appleby <colten.appleby@gmail.com>
2021-04-28 12:38:54 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+
+    function blurEvent() {
+        console.log("Good!")
+    }
+
+    function focusEvent() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    return (
+        <div> 
+            <button onBlur={blurEvent} onFocus={focusEvent} > Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+function Keypad () {
+
+    function change(event) {
+        console.log("Entering password...")
+    }
+
+    return (
+        <div> 
+            <input type="password" onChange={change} />
+        </div>
+    )
+}
+
+export default Keypad;
